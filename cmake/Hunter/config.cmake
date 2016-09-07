a8eafc1a0b65dc8d0681326d449d3a079c532f28
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-09-07 10:12:39 +0300

    Add cmake/Hunter/config.cmake example

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,4 @@
+# 3rd party tuning:
+# * https://github.com/ruslo/hunter/wiki/example.custom.config.id
+# NOTE: current setting doesn't change anything, it's just an example
+hunter_config(GTest VERSION ${HUNTER_GTest_VERSION})
