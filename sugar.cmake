068ebdfe5c3b96e441f5829fb92c07a725810e74
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-01-23 16:13:18 +0300

    Remove top level 'sugar.cmake'

--- sugar.cmake
+++
@@ -1,9 +0,0 @@
-sugar_include(lib)
-sugar_include(app)
-
-# Folder 'test' have no sources. All sources for GTest located in 'lib' folder.
-# Sugar will collect only data files for testing.
-sugar_include(test)
-
-# Do not dive into 'example' folder. Projects can be used stand-alone
-# hence should collect sources themself.
